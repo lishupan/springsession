7ad53559d89aa6711741f91c3df274752ed419bd
will <1778047278@qq.com>
2020-12-23 16:16:38 +0800

    Initial commit

---
+++ src/main/java/com/offcn/controller/HelloSessionController.java
@@ -0,0 +1,24 @@
+package com.offcn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class HelloSessionController {
+
+    //写入数据到session
+    @RequestMapping("set")
+    public String setSession(String msg, HttpSession session){
+        session.setAttribute("msg",msg);
+        return "session-set-ok";
+    }
+
+    //读取session数据
+    @RequestMapping("get")
+    public String getSession(HttpSession session){
+        String msg = (String) session.getAttribute("msg");
+        return "get-session:"+msg;
+    }
+}
